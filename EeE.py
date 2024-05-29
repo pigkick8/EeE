e8c6d99cc8cee895fedb27a59f7f33a63e7c9b02
pgmrDohan <dohan.k764@gmail.com>
2024-05-29 21:02:37 +0900

    바위

--- EeE.py
+++ EeE.py
@@ -4,5 +4,11 @@ player2 = input("player2 가위, 바위, 보 입력\n")
 if player1 == '가위':
 
 elif player1 == "바위":
+	if player2 == "바위":
+		print("player1, player2 비김!!")
+	elif player2 == "가위":
+		print("player1 승리")
+	else:
+		print("player2 승리")
 
 else:
